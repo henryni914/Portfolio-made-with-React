405c3f2403e19adacce1e151365bd830d00322ef
Henry Ni <henryni914@gmail.com>
2020-03-30 21:31:09 -0400

    started creating navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import NavBar from "./components/NavBar";
 import Home from './pages/Home';
 import Project from './pages/Project'
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -8,10 +9,11 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-    <div className="App">
-    <Route exact path="/" component={Home} />
-    <Route exact path="/projects" component={Projects} />
-    </div>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/project" component={Project} />
+      </div>
     </Router>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function NavBar() {
+
+    const location = useLocation();
+
+    return (
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                    Home
+                </Link>
+            </li>
+        </ul>
+    )
+}
+
+export default NavBar;
