97e43db8f47bdb72bb4383eec4e07166a5d72904
Henry Ni <henryni914@gmail.com>
2020-03-30 23:43:50 -0400

    added another prop and a new project

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-function Card() {
+function Card(props) {
     return (
         < div >
-
+            <div className="card mt-4 mr-2 text-center" style={{width: "18rem"}}>
+                {/* <img src="..." className="card-img-top" alt="..." /> */}
+                <div className="card-body">
+                    <h5 className="card-title">{props.name}</h5>
+                    <p className="card-text">{props.description}</p>
+                </div>
+                <ul className="list-group list-group-flush" style={{fontSize: "20px"}}>
+                    Technologies Used
+                    {props.technologies.map( element => (
+                        <li className="list-group-item" style={{fontSize: "15px"}}>{element}</li>
+                    ))}
+                </ul>
+                <div className="card-body">
+                    <a href={props.github} className="card-link">Github Link</a>
+                    <a href={props.demo} className="card-link">Demo Link</a>
+                </div>
+            </div>
         </div >
 
     )
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Card from "../components/Card"
+import Projects from "../projects.json"
 
-function Project () {
-
-
+function Project() {
 
     return (
         <div>
             <div className="container">
-                <Card />
-                Hello!
+                <div className="row">
+                    {Projects.map(project => (
+                        <Card
+                            name={project.name}
+                            id={project.id}
+                            description={project.description}
+                            github={project.githubLink}
+                            demo={project.demoLink}
+                            technologies={project.technologies}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     )
