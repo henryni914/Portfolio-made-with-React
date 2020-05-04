f5de85bc7f35857468b6b93d4998858d48bd9f1f
Henry Ni <henryni914@gmail.com>
2020-05-03 23:20:21 -0400

    changed format

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,9 @@ export default function Home() {
     return (
         <>
             <NavBar />
-            <Jumbotron />
             <ProjectCarousel />
+            <Jumbotron />
+            
         </>
     )
 }
