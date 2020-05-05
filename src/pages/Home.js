c4030e258c746e1f6f66bd847a9355236f9a3aea
Henry Ni <henryni914@gmail.com>
2020-05-04 23:26:54 -0400

    added card for skills

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import NavBar from '../components/NavBar'
 import Jumbotron from '../components/Jumbotron'
 import ProjectCarousel from '../components/ProjectCarousel'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 export default function Home() {
 
@@ -9,8 +11,16 @@ export default function Home() {
         <>
             <NavBar />
             <ProjectCarousel />
+            <Card style={{ width: '18rem' }}>
+                <Card.Header>Featured</Card.Header>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>JavaScript</ListGroup.Item>
+                    <ListGroup.Item>React</ListGroup.Item>
+                    <ListGroup.Item>Mongo</ListGroup.Item>
+                </ListGroup>
+            </Card>
             <Jumbotron />
-            
+
         </>
     )
 }
