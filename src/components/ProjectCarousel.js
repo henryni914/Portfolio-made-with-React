930da7fc5bfb021785cb179af1b5cbb18d864ab8
Henry Ni <henryni914@gmail.com>
2020-05-05 18:05:47 -0400

    sample carousel with cards

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-// import Container from 'react-bootstrap/Container'
 import Carousel from 'react-bootstrap/Carousel'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+// import CardBody from 'react-bootstrap/CardImg'
+// import CardTitle from'react-bootstrap/CardTitle'
+// import CardText from 'react-bootstrap/CardText'
+import Button from 'react-bootstrap/Button'
 
 export default function ProjectCarousel() {
 
@@ -11,10 +16,70 @@ export default function ProjectCarousel() {
     };
 
     return (
-     
-            <Carousel activeIndex={index} onSelect={handleSelect} fluid>
-                <Carousel.Item>
-                    <img
+
+        <Carousel className='carousel-multi-item' activeIndex={index} onSelect={handleSelect} fluid>
+            <Carousel.Item align='center'>
+                <Row align='center'>
+                    <Card className='col-md-3 mb-2 ml-5'>
+                        <Card.Img variant="top" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+                            alt="First slide">
+                        </Card.Img>
+                        <Card.Body>
+                            <Card.Title>
+                                DonorSpot
+                        </Card.Title>
+                            <Card.Text>
+                                This is donorspot.
+                        </Card.Text>
+                        <Button className='ml-2'>
+                                Github
+                        </Button>
+                            <Button className='ml-2'>
+                                Demo
+                        </Button>
+                        </Card.Body>
+                    </Card>
+                    <Card className='col-md-3 mb-2 ml-5'>
+                        <Card.Img variant="top" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+                            alt="First slide">
+                        </Card.Img>
+                        <Card.Body>
+                            <Card.Title>
+                                DonorSpot
+                        </Card.Title>
+                            <Card.Text>
+                                This is donorspot.
+                        </Card.Text>
+                            <Button className='ml-2'>
+                                Github
+                        </Button>
+                            <Button className='ml-2'>
+                                Demo
+                        </Button>
+                        </Card.Body>
+                    </Card>
+                    <Card className='col-md-3 mb-2 ml-5'>
+                        <Card.Img variant="top" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+                            alt="First slide">
+                        </Card.Img>
+                        <Card.Body>
+                            <Card.Title>
+                                DonorSpot
+                        </Card.Title>
+                            <Card.Text>
+                                This is donorspot.
+                        </Card.Text>
+                        <Button className='ml-2'>
+                                Github
+                        </Button>
+                            <Button className='ml-2'>
+                                Demo
+                        </Button>
+                        </Card.Body>
+                    </Card>
+                </Row>
+            </Carousel.Item>
+            {/* <img
                         className="d-block w-100"
                         // height='500'
                         src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
@@ -23,36 +88,35 @@ export default function ProjectCarousel() {
                     <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        // height='500'
-                        src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
-                        alt="Second slide"
-                    />
+                    </Carousel.Caption> */}
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    // height='500'
+                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+                    alt="Second slide"
+                />
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        // height='500'
-                        src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
-                        alt="Third slide"
-                    />
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    // height='500'
+                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+                    alt="Third slide"
+                />
 
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,25 +2,14 @@ import React from 'react';
 import NavBar from '../components/NavBar'
 import Jumbotron from '../components/Jumbotron'
 import ProjectCarousel from '../components/ProjectCarousel'
-import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup'
 
 export default function Home() {
 
     return (
         <>
             <NavBar />
-            <ProjectCarousel />
-            <Card style={{ width: '18rem' }}>
-                <Card.Header>Featured</Card.Header>
-                <ListGroup variant="flush">
-                    <ListGroup.Item>JavaScript</ListGroup.Item>
-                    <ListGroup.Item>React</ListGroup.Item>
-                    <ListGroup.Item>Mongo</ListGroup.Item>
-                </ListGroup>
-            </Card>
             <Jumbotron />
-
+            <ProjectCarousel />
         </>
     )
 }
