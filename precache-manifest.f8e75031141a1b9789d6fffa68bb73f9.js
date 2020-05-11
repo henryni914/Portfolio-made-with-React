d758e04b379cd84a958341693bafb21c0277505e
Henry Ni <henryni914@gmail.com>
2020-05-11 17:45:32 -0400

    Updates

---
+++ precache-manifest.f8e75031141a1b9789d6fffa68bb73f9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f7f509423e31585f83d4e9a8a90dd3",
+    "url": "/Portfolio-made-with-React/index.html"
+  },
+  {
+    "revision": "b2b76b092649e7889984",
+    "url": "/Portfolio-made-with-React/static/css/main.5a6b043f.chunk.css"
+  },
+  {
+    "revision": "b534f5649ca9e41bd37d",
+    "url": "/Portfolio-made-with-React/static/js/2.8daac3c0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio-made-with-React/static/js/2.8daac3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2b76b092649e7889984",
+    "url": "/Portfolio-made-with-React/static/js/main.68b9e9bc.chunk.js"
+  },
+  {
+    "revision": "45a922b2118e6368802e",
+    "url": "/Portfolio-made-with-React/static/js/runtime-main.315eb77a.js"
+  },
+  {
+    "revision": "336a81925eaa6761eea33e202d880bad",
+    "url": "/Portfolio-made-with-React/static/media/AddToCart.336a8192.png"
+  },
+  {
+    "revision": "8f42f0167643b8d049a6cd0f650abd2d",
+    "url": "/Portfolio-made-with-React/static/media/Cart.8f42f016.png"
+  },
+  {
+    "revision": "1ecfcca5556ca31084aadf1016c3a159",
+    "url": "/Portfolio-made-with-React/static/media/CharityPage.1ecfcca5.png"
+  },
+  {
+    "revision": "911c036ec46a040cf2d2e5753e042f2f",
+    "url": "/Portfolio-made-with-React/static/media/DashBottom.911c036e.png"
+  },
+  {
+    "revision": "d8171b8f623e948868535f99f0d4b6fc",
+    "url": "/Portfolio-made-with-React/static/media/DashTop.d8171b8f.png"
+  },
+  {
+    "revision": "e3a6652b74f418f3f11b49e80d842a38",
+    "url": "/Portfolio-made-with-React/static/media/DonorSpot Home.e3a6652b.png"
+  }
+]);
