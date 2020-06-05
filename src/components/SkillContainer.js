c4a691a74005b5309dda11951869c23a3267701c
Henry Ni <henryni914@gmail.com>
2020-06-04 22:41:25 -0400

    testing image/icon in skill cards

--- src/components/SkillContainer.js
+++ src/components/SkillContainer.js
@@ -5,13 +5,14 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
+import charityPage from '../images/CharityPage.png'
 
 export default function SkillContainer() {
     return (
         <Container>
             <CardDeck className="text-center">
                 <Card className="mt-1 mb-1">
-                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Img variant="top" src={charityPage} />
                     <Card.Body>
                         <Card.Title>NodeJS</Card.Title>
                     </Card.Body>
@@ -27,9 +28,8 @@ export default function SkillContainer() {
                     <Card.Body>
                         <Card.Title>JavaScript</Card.Title>
                     </Card.Body>
-                    <Card.Footer>
-                        {/* <small className="text-muted">Last updated 3 mins ago</small> */}
-                    </Card.Footer>
+                    {/* <Card.Footer>
+                    </Card.Footer> */}
                 </Card>
                 <Card className="mt-1 mb-1">
                     <Card.Img variant="top" src="holder.js/100px160" />
@@ -63,9 +63,6 @@ export default function SkillContainer() {
                     <Card.Body>
                         <Card.Title>JavaScript</Card.Title>
                     </Card.Body>
-                    <Card.Footer>
-                        {/* <small className="text-muted">Last updated 3 mins ago</small> */}
-                    </Card.Footer>
                 </Card>
                 <Card className="mt-1 mb-1">
                     <Card.Img variant="top" src="holder.js/100px160" />
