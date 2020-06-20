803009ba59753300f3b2e8dd6c6c650cfc9fdd10
Henry Ni <henryni914@gmail.com>
2020-06-19 21:08:50 -0400

    re-added e-mail

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -33,7 +33,7 @@ color: white;
                             Hello, I am a full stack web developer with a background in JavaScript, React, Node.js, MongoDB, MySQL, Bootstrap, and more. I enjoy the challenges of learning new technologies and languages in order to create modern and functional web applications. Please see below for links to my resume, projects, and contact information.
                     </p>
                         <p>
-                            {/* Email: ni.henry914@gmail.com */}
+                            Email: ni.henry914@gmail.com
                     </p>
                         <p>
                             <Button variant="primary" className='mr-2 mt-2' href="https://github.com/henryni914" target="_blank">Github</Button>
